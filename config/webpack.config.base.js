3eb22fa89e8b340bcf4106ef57e163ecdfa7548f
squallvince <squallvince@hotmail.com>
2018-05-16 16:57:17 +0800

    adjust indent

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -99,7 +99,7 @@ module.exports = {
         exclude: /node_modules/,
         use: [{
           loader: 'babel-loader'
-          }],
+        }],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
